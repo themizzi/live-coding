f1e533ae969d39b30a94653bf9c706bc0a0a7604
Joe Mizzi <themizzi@me.com>
2020-12-14 20:52:23 -0600

    chore: add gts (#5)

--- web-app/src/app/app.component.spec.ts
+++ web-app/src/app/app.component.spec.ts
@@ -1,17 +1,13 @@
-import { TestBed } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
-import { AppComponent } from './app.component';
+import {TestBed} from '@angular/core/testing';
+import {RouterTestingModule} from '@angular/router/testing';
+import {AppComponent} from './app.component';
 
 describe('AppComponent', () => {
   it('should create the app', async () => {
     // GIVEN
     await TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
-      declarations: [
-        AppComponent
-      ],
+      imports: [RouterTestingModule],
+      declarations: [AppComponent],
     }).compileComponents();
 
     // WHEN
