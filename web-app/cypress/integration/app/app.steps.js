97afa8aa8b201515009932f3ece01b3b5bdd1fbd
Joe Mizzi <themizzi@me.com>
2020-12-15 18:49:01 -0600

    chore: setup cypress (#7)

---
+++ web-app/cypress/integration/app/app.steps.js
@@ -0,0 +1,17 @@
+import { Then, When, Before } from 'cypress-cucumber-preprocessor/steps';
+
+Before(() => {
+  Cypress.on('window:before:load', win => {
+    cy.spy(win.console, 'error');
+  });
+});
+
+When('I visit the homepage', () => {
+  cy.visit('');
+});
+
+Then('there are no errors', () => {
+  cy.window().then(win => {
+    expect(win.console.error).to.have.callCount(0);
+  });
+});
