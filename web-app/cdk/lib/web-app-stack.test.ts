25d004cd533c719bfbf2f126a1b3361e790336da
Joe Mizzi <themizzi@me.com>
2020-12-26 16:51:40 -0600

    chore: rename stack and make tests siblings

--- web-app/cdk/lib/web-app-stack.test.ts
+++ web-app/cdk/lib/web-app-stack.test.ts
@@ -1,11 +1,14 @@
 import {expect as expectCDK, matchTemplate, MatchStyle} from '@aws-cdk/assert';
 import * as cdk from '@aws-cdk/core';
-import * as Cdk from '../lib/cdk-stack';
+import * as Cdk from './web-app-stack';
 
 test('Empty Stack', () => {
+  // GIVEN
   const app = new cdk.App();
+
   // WHEN
   const stack = new Cdk.CdkStack(app, 'MyTestStack');
+
   // THEN
   expectCDK(stack).to(
     matchTemplate(
