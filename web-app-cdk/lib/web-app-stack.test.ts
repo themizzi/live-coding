521c2871c4df919bcf7b392233ee5df442c863aa
Joe Mizzi <themizzi@me.com>
2020-12-26 19:43:36 -0600

    ci: deploy to s3 and cloudfront

--- web-app-cdk/lib/web-app-stack.test.ts
+++ web-app-cdk/lib/web-app-stack.test.ts
@@ -1,21 +1,330 @@
-import {expect as expectCDK, matchTemplate, MatchStyle} from '@aws-cdk/assert';
-import {App} from '@aws-cdk/core';
-import {WebAppStack} from './web-app-stack';
+import {
+  expect as expectCDK,
+  matchTemplate,
+  MatchStyle,
+  haveResourceLike,
+  ResourcePart,
+  haveOutput,
+} from '@aws-cdk/assert';
+import {CfnDistribution} from '@aws-cdk/aws-cloudfront';
+import {CfnBucket} from '@aws-cdk/aws-s3';
+import {App, Fn, RemovalPolicy} from '@aws-cdk/core';
+import {WebAppStack, WebAppStackProps} from './web-app-stack';
 
-test('Empty Stack', () => {
-  // GIVEN
-  const app = new App();
+describe('Web App Stack', () => {
+  let app: App;
 
-  // WHEN
-  const stack = new WebAppStack(app, 'MyTestStack');
+  const testStack = (props?: WebAppStackProps): WebAppStack => {
+    return new WebAppStack(app, 'MyTestStack', props);
+  };
 
-  // THEN
-  expectCDK(stack).to(
-    matchTemplate(
-      {
-        Resources: {},
+  beforeEach(() => {
+    app = new App();
+  });
+
+  test('Empty Stack', () => {
+    // WHEN
+    const stack = testStack();
+
+    // THEN
+    expectCDK(stack).to(
+      matchTemplate(
+        {
+          Resources: {},
+        },
+        MatchStyle.EXACT
+      )
+    );
+  });
+
+  [undefined, RemovalPolicy.DESTROY].forEach(x => {
+    test('Has bucket with removal policy', () => {
+      // GIVEN
+      const removalPolicy = x === RemovalPolicy.DESTROY ? 'Delete' : undefined;
+
+      // WHEN
+      const stack = testStack({
+        bucket: {
+          removalPolicy: x,
+        },
+      });
+
+      // THEN
+      expectCDK(stack).to(
+        haveResourceLike(
+          'AWS::S3::Bucket',
+          {
+            DeletionPolicy: removalPolicy,
+          },
+          ResourcePart.CompleteDefinition
+        )
+      );
+    });
+  });
+
+  test('Restricts public access', () => {
+    // WHEN
+    const stack = testStack({
+      bucket: {},
+    });
+
+    // THEN
+    expectCDK(stack).to(
+      haveResourceLike('AWS::S3::Bucket', {
+        PublicAccessBlockConfiguration: {
+          BlockPublicAcls: true,
+          BlockPublicPolicy: true,
+          IgnorePublicAcls: true,
+          RestrictPublicBuckets: true,
+        },
+      })
+    );
+  });
+
+  test('Outputs bucket arn', () => {
+    // WHEN
+    const stack = testStack({
+      bucket: {},
+    });
+
+    // THEN
+    const bucket = stack.bucket.node.defaultChild as CfnBucket;
+    const logicalId = stack.resolve(bucket.logicalId);
+    expectCDK(stack).to(
+      haveOutput({
+        outputName: 'BucketARN',
+        outputValue: stack.resolve(Fn.getAtt(logicalId, 'Arn').toString()),
+      })
+    );
+  });
+
+  test('Outputs bucket name', () => {
+    // WHEN
+    const stack = testStack({
+      bucket: {},
+    });
+
+    // THEN
+    const bucket = stack.bucket.node.defaultChild as CfnBucket;
+    const logicalId = stack.resolve(bucket.logicalId);
+    expectCDK(stack).to(
+      haveOutput({
+        outputName: 'BucketName',
+        outputValue: stack.resolve(Fn.ref(logicalId)),
+      })
+    );
+  });
+
+  test('Has distribution with price class 100', () => {
+    // WHEN
+    const stack = testStack({
+      bucket: {},
+    });
+
+    // THEN
+    expectCDK(stack).to(
+      haveResourceLike('AWS::CloudFront::Distribution', {
+        DistributionConfig: {
+          PriceClass: 'PriceClass_100',
+        },
+      })
+    );
+  });
+
+  test('Has default root object of index.html', () => {
+    // WHEN
+    const stack = testStack({
+      bucket: {},
+    });
+
+    // THEN
+    expectCDK(stack).to(
+      haveResourceLike('AWS::CloudFront::Distribution', {
+        DistributionConfig: {
+          DefaultRootObject: 'index.html',
+        },
+      })
+    );
+  });
+
+  test('Has bucket origin', () => {
+    // WHEN
+    const stack = testStack({
+      bucket: {},
+    });
+
+    // THEN
+    const bucket = stack.bucket.node.defaultChild as CfnBucket;
+    const logicalId = stack.resolve(bucket.logicalId);
+    expectCDK(stack).to(
+      haveResourceLike('AWS::CloudFront::Distribution', {
+        DistributionConfig: {
+          Origins: [
+            {
+              DomainName: stack.resolve(
+                Fn.getAtt(logicalId, 'RegionalDomainName')
+              ),
+            },
+          ],
+        },
+      })
+    );
+  });
+
+  test('Has default origin path', () => {
+    // WHEN
+    const stack = testStack({
+      bucket: {},
+    });
+
+    // THEN
+    expectCDK(stack).to(
+      haveResourceLike('AWS::CloudFront::Distribution', {
+        DistributionConfig: {
+          Origins: [
+            {
+              OriginPath: '/default',
+            },
+          ],
+        },
+      })
+    );
+  });
+
+  test('Uses supplied origin path', () => {
+    // WHEN
+    const stack = testStack({
+      bucket: {
+        prefix: 'prefix',
       },
-      MatchStyle.EXACT
-    )
-  );
+    });
+
+    // THEN
+    expectCDK(stack).to(
+      haveResourceLike('AWS::CloudFront::Distribution', {
+        DistributionConfig: {
+          Origins: [
+            {
+              OriginPath: '/prefix',
+            },
+          ],
+        },
+      })
+    );
+  });
+
+  test('Has viewer policy of https redirect', () => {
+    // WHEN
+    const stack = testStack({
+      bucket: {
+        prefix: 'prefix',
+      },
+    });
+
+    // THEN
+    expectCDK(stack).to(
+      haveResourceLike('AWS::CloudFront::Distribution', {
+        DistributionConfig: {
+          DefaultCacheBehavior: {
+            ViewerProtocolPolicy: 'redirect-to-https',
+          },
+        },
+      })
+    );
+  });
+
+  test('Outputs distribution domain name', () => {
+    // WHEN
+    const stack = testStack({
+      bucket: {},
+    });
+
+    // THEN
+    const distribution = stack.distribution.node
+      .defaultChild as CfnDistribution;
+    const logicalId = stack.resolve(distribution.logicalId);
+    expectCDK(stack).to(
+      haveOutput({
+        outputName: 'Domain',
+        outputValue: stack.resolve(Fn.getAtt(logicalId, 'DomainName')),
+      })
+    );
+  });
+
+  test('Has bucket deployment with distribution', () => {
+    // WHEN
+    const stack = testStack({
+      bucket: {},
+    });
+
+    // THEN
+    const distribution = stack.distribution.node
+      .defaultChild as CfnDistribution;
+    const logicalId = stack.resolve(distribution.logicalId);
+    expectCDK(stack).to(
+      haveResourceLike('Custom::CDKBucketDeployment', {
+        DistributionId: stack.resolve(Fn.ref(logicalId)),
+      })
+    );
+  });
+
+  test('Has bucket deployment with distribution paths', () => {
+    // WHEN
+    const stack = testStack({
+      bucket: {},
+    });
+
+    // THEN
+    expectCDK(stack).to(
+      haveResourceLike('Custom::CDKBucketDeployment', {
+        DistributionPaths: ['/*'],
+      })
+    );
+  });
+
+  test('Has bucket deployment with bucket', () => {
+    // WHEN
+    const stack = testStack({
+      bucket: {},
+    });
+
+    // THEN
+    const bucket = stack.bucket.node.defaultChild as CfnBucket;
+    const logicalId = stack.resolve(bucket.logicalId);
+    expectCDK(stack).to(
+      haveResourceLike('Custom::CDKBucketDeployment', {
+        DestinationBucketName: stack.resolve(Fn.ref(logicalId)),
+      })
+    );
+  });
+
+  test('Has bucket deployment with bucket default prefix', () => {
+    // WHEN
+    const stack = testStack({
+      bucket: {},
+    });
+
+    // THEN
+    expectCDK(stack).to(
+      haveResourceLike('Custom::CDKBucketDeployment', {
+        DestinationBucketKeyPrefix: 'default',
+      })
+    );
+  });
+
+  test('Has bucket deployment with bucket supplied prefix', () => {
+    // WHEN
+    const stack = testStack({
+      bucket: {
+        prefix: 'prefix',
+      },
+    });
+
+    // THEN
+    expectCDK(stack).to(
+      haveResourceLike('Custom::CDKBucketDeployment', {
+        DestinationBucketKeyPrefix: 'prefix',
+      })
+    );
+  });
 });
--- web-app-cdk/lib/web-app-stack.ts
+++ web-app-cdk/lib/web-app-stack.ts
@@ -1,9 +1,83 @@
-import * as cdk from '@aws-cdk/core';
+import {
+  Distribution,
+  IDistribution,
+  PriceClass,
+  ViewerProtocolPolicy,
+} from '@aws-cdk/aws-cloudfront';
+import {S3Origin} from '@aws-cdk/aws-cloudfront-origins';
+import {BlockPublicAccess, Bucket, IBucket} from '@aws-cdk/aws-s3';
+import {BucketDeployment, Source} from '@aws-cdk/aws-s3-deployment';
+import {
+  CfnOutput,
+  Construct,
+  RemovalPolicy,
+  Stack,
+  StackProps,
+} from '@aws-cdk/core';
+import {join} from 'path';
 
-export class WebAppStack extends cdk.Stack {
-  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+export interface WebAppStackProps extends StackProps {
+  bucket?: {
+    bucketArn?: string;
+    prefix?: string;
+    removalPolicy?: RemovalPolicy;
+    sourcePath?: string;
+  };
+}
+export class WebAppStack extends Stack {
+  readonly bucket: IBucket;
+  readonly distribution: IDistribution;
+
+  constructor(scope: Construct, id: string, props?: WebAppStackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    if (props?.bucket) {
+      this.bucket = props.bucket.bucketArn
+        ? Bucket.fromBucketArn(
+            this,
+            'WebAppStackBucket',
+            props.bucket.bucketArn
+          )
+        : new Bucket(this, 'WebAppStackBucket', {
+            removalPolicy: props.bucket.removalPolicy ?? RemovalPolicy.RETAIN,
+            blockPublicAccess: BlockPublicAccess.BLOCK_ALL,
+          });
+
+      new CfnOutput(this, 'BucketARN', {
+        value: this.bucket.bucketArn,
+      });
+
+      new CfnOutput(this, 'BucketName', {
+        value: this.bucket.bucketName,
+      });
+
+      this.distribution = new Distribution(this, 'WebAppDistribution', {
+        priceClass: PriceClass.PRICE_CLASS_100,
+        defaultRootObject: 'index.html',
+        defaultBehavior: {
+          origin: new S3Origin(this.bucket, {
+            originPath: `/${props.bucket.prefix ?? 'default'}`,
+          }),
+          viewerProtocolPolicy: ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+        },
+      });
+
+      new CfnOutput(this, 'Domain', {
+        value: this.distribution.distributionDomainName,
+      });
+
+      new BucketDeployment(this, 'WebAppDeployment', {
+        sources: [
+          Source.asset(
+            props.bucket.sourcePath ??
+              join(__dirname, '..', '..', 'web-app', 'dist', 'web-app')
+          ),
+        ],
+        distribution: this.distribution,
+        distributionPaths: ['/*'],
+        destinationBucket: this.bucket,
+        destinationKeyPrefix: props.bucket.prefix ?? 'default',
+      });
+    }
   }
 }
